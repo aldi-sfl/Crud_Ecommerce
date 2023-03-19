d42e265c2b21f01a75f11c367773a1d7ed45cacd
Aldi <20sa1132@amikompurwokert.ac.id>
2023-03-20 01:06:37 +0700

    first commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.main')
+@section('carousel')
+<div class="carousel"></div>
+    <h2 class="head-text">
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad neque explicabo inventore alias itaque, nesciunt sunt dolorem sapiente, sequi facilis veniam at libero, doloremque deserunt ipsam nisi voluptatibus harum blanditiis? Harum cum eius est odit totam rerum. In a doloremque, consequatur nisi eius recusandae suscipit tempora possimus odio magni unde atque debitis itaque voluptas illo? Dignissimos, corrupti impedit. Iure non dolorem possimus, rem expedita praesentium soluta est, commodi ab, eos sed quia fuga mollitia quisquam! Quia sunt, placeat distinctio eligendi nulla, quidem totam delectus rem error quaerat, sint libero. Placeat aliquid labore officia sed quam optio quae nemo possimus minima.</p>
+    </h2>
+</div>
+    
+@endsection
+
+@section('home')
+<h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, labore!</h1>
+    
+@endsection
---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,27 @@
+<nav>
+    <div class="nav-content">
+      <div class="logo">
+        <lord-icon
+        src="https://cdn.lordicon.com/myroguwc.json"
+        style="width:60px;height:60px;">
+      </lord-icon>
+      <a href="#">ASL Computer</a>
+      </div>
+      <form class="search-box">
+        <input type="text" placeholder=" Cari produk....">
+        <button type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+      </form>
+      <ul class="nav-links">
+        <li><a href=""><lord-icon
+          src="https://cdn.lordicon.com/rmzhcgbh.json"
+          trigger="hover"
+          style="width:40px;height:40px">
+          </lord-icon></a></li>
+        <li class="link"><a href="#">Login</a></li>
+        <li class="link"><a href="#">Register</a></li>
+      </ul>
+    </div>
+  </nav>
+
+
+  
