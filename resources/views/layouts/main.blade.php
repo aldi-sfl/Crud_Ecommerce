7b807f04b0239dc0f00563551d9455e4518d1b49
Aldi <20sa1132@amikompurwokert.ac.id>
2023-03-22 14:18:13 +0700

    styling web

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -13,15 +13,9 @@
 </head>
 <body>
     @include('partials.navbar')
-    <section class="home">
-        @yield('carousel')
-    </section>
-    <div class="text">
     
-            @yield('home')
-    </div>
-
-
+    @yield('home')
+   
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
 </html>
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,27 +1,24 @@
 <nav>
-    <div class="nav-content">
-      <div class="logo">
-        <lord-icon
-        src="https://cdn.lordicon.com/myroguwc.json"
-        style="width:60px;height:60px;">
-      </lord-icon>
-      <a href="#">ASL Computer</a>
-      </div>
-      <form class="search-box">
-        <input type="text" placeholder=" Cari produk....">
-        <button type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
-      </form>
-      <ul class="nav-links">
-        <li><a href=""><lord-icon
-          src="https://cdn.lordicon.com/rmzhcgbh.json"
-          trigger="hover"
-          style="width:40px;height:40px">
-          </lord-icon></a></li>
-        <li class="link"><a href="#">Login</a></li>
-        <li class="link"><a href="#">Register</a></li>
-      </ul>
-    </div>
-  </nav>
+   <div class="nav-content">
+     <div class="logo">
+     <img src="img/logo/laptop.png" alt="" style="width:60px;height:60px;">
+     <a href="#" style="color:#00214d">ASL Comp</a>
+     </div>
+     <form class="search-box">
+       <input type="text" placeholder=" Cari produk....">
+       <button type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+     </form>
+     <ul class="nav-links">
+       <li><a href=""><lord-icon
+         src="https://cdn.lordicon.com/rmzhcgbh.json"
+         trigger="hover"
+         style="width:40px;height:40px">
+         </lord-icon></a></li>
+       <li class="link"><a href="#">Login</a></li>
+       <li class="link"><a href="#">Register</a></li>
+     </ul>
+   </div>
+ </nav>
 
 
-  
+ 
